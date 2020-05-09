fe50efaa548284fc12809b6a6456e3aaf143f708
gusaguillon <gusaguillon@gmail.com>
2020-05-08 22:01:34 -0500

    modificaion index

---
+++ index.js
@@ -0,0 +1,17 @@
+const    getAsteroidsAndres = require("./andres.js")
+getAsteroidsAndres("1984-10-24", "1984-10-24")
+
+const    getAsteroidsNatasha = require("./natasha.js")
+getAsteroidsNatasha("1995-05-27", "1995-05-27")
+
+const    getAsteroidsJuan = require("./juan.js")
+getAsteroidsJuan("1996-05-27", "1996-05-27")
+
+const    getAsteroidsIsrael = require("./israel.js")
+getAsteroidsIsrael("1990-01-20", "1990-01-20")
+
+
+// name
+// diametro max y Min km
+// potencialmente peligro
+
