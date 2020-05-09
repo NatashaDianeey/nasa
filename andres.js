9444b4f7bf115c942554dbc2ee2a6cf383da285b
gusaguillon <gusaguillon@gmail.com>
2020-05-08 22:05:02 -0500

    Cambios Andres

---
+++ andres.js
@@ -0,0 +1,37 @@
+const axios = require("axios");
+function getAsteroids(start, end){
+    const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${start}&end_date=${end}&api_key=Slt8vHnSfn6bkcrQ1JkUmassBCxo497kV9obExog`
+    axios.get(url)
+    .then((asteroides) =>{
+        return asteroides.data
+    })
+    .then((asteroides) =>{
+        // console.log(asteroides.near_earth_objects)
+        return asteroides.near_earth_objects        
+    })
+    .then((asteroides) =>{
+        console.log(asteroides[start][0].name)
+         
+        
+         
+
+        
+    })
+    .catch((errores) =>{
+        console.log(errores)
+    })
+
+
+
+
+
+
+
+}
+
+getAsteroids("1984-10-24", "1984-10-24")
+
+ 
+
+// conso
+//     module.exports = getAsteroids
